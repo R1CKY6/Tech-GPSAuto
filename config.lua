0f64303a09bc25533f4edb5c0d5b9e2b28e488f9
R1CKY6 <100082886+R1CKY6@users.noreply.github.com>
2022-06-02 19:52:21 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,3 +12,14 @@ Config.Sprite = 56
 Config.BlipColor = 5
 Config.BlipSize = 1.0
 Config.BlipName = "GPS Auto"
+
+
+
+Ricky_Lang =  {
+    ['title_menu'] = "Menu GPS",
+    ['menu_option'] = "Attacca/Rimuovi GPS",
+    ['nessun_veicolo_vicino'] = "Devi essere vicino ad un veicolo",
+    ['gps_attaccato'] = "GPS Attaccato",
+    ['gps_tolto'] = "Stai rimuovendo il GPS",
+    ['auto_sbagliata'] = "Non sei vicino all\'auto giusta",
+}
